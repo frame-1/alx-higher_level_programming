5515b91d266444c35cd41bb045b2285a41cffa99
frame-1 <muthee031@gmail.com>
2022-06-21 15:29:36 -0700

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
+"""An empty class Square that defines a square."""
+
 class Square:
-    """Class Square that defines a square.
-    """
+    """Class Square that defines a square is empty."""
     pass
