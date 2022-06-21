e74ccbb78a8744e7d416444d3070f09a5c05c0f9
frame-1 <muthee031@gmail.com>
2022-06-21 15:37:37 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,20 @@
     """Class Square that defines a square object
     """
 
-class Square:
 
+class Square:
+    """Module of a square
+
+    Attributes:
+        __size (int): Describes the length of a square
+    """
     def __init__(self, size):
         """Initialize method that stores the size of the square
+
+
         Args:
             param1 (int): size of the square
+
 
         Returns:
             none
