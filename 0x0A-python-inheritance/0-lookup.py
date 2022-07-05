2909a633962c161b66e2b7358515721a29a2c3b5
frame-1 <muthee031@gmail.com>
2022-07-04 18:25:37 -0700

    Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+!/usr/bin/python3
+"""A function that returns a list object"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return(dir(obj))
